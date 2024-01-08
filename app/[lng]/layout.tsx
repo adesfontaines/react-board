9f8aa2f20bac676807fac325c97361c99e42e793
Antonin DESFONTAINES <antonin.desfontaines@outlook.com>
2024-01-08 18:09:35 +0100

    Remove loading spinner

--- app/[lng]/layout.tsx
+++ app/[lng]/layout.tsx
@@ -57,7 +57,6 @@ export default function RootLayout({
             crawlSpeed={200}
             height={3}
             crawl={true}
-            showSpinner={true}
             easing="ease"
             speed={200}
             shadow="0 0 10px #2299DD,0 0 5px #2299DD"
